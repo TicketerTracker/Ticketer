0f9975ce775d27925f99867a60ea7d1573bef7f8
LucasRosenberger <lucas.rosenberger@gmx.at>
2021-02-03 14:14:35 +0000

    implemente register functionality firebase

--- Client/ticketer/src/app/authentification/authentification.component.ts
+++ Client/ticketer/src/app/authentification/authentification.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationServicesService } from 'src/services/authentication-services.service';
 
 @Component({
   selector: 'app-authentification',
@@ -13,20 +14,19 @@ export class AuthentificationComponent implements OnInit {
   public passwordConfirm: string;
 
 
-  constructor() { }
+  constructor(private authService: AuthenticationServicesService) { }
 
   ngOnInit() { }
 
   public register() {
     if (this.password != this.passwordConfirm) {
-      return alert("wrong password");
-    }
-    if (this.password === "" || this.passwordConfirm === "" || this.mailadress === "" || this.username === "") {
-      return alert("type in every value");
-    }
-    if (this.mailadress && this.mailadress.indexOf("@") === -1) {
-      return alert("please enter valid mail adress");
+      return alert("Passwords do not match");
+    } else if (this.password === "" || this.username === "") {
+      return alert("Missing Username or Password");
+    } else if (!this.mailadress || this.mailadress.indexOf("@") === -1) {
+      return alert("Mssing Mail Adress");
     }
 
+    this.authService.createUserWithEmailAndPassword(this.mailadress, this.password);
   }
 }
