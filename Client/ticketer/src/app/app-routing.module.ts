1bf764ca7e13f806d034f6d88a3ab508b1efa777
Max <maximilian.hoeller@gmail.com>
2021-02-03 16:13:41 +0100

    mainPage

--- Client/ticketer/src/app/app-routing.module.ts
+++ Client/ticketer/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { MainpageComponent } from './mainpage/mainpage.component';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
@@ -5,13 +6,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },{path: 'register', component: AuthentificationComponent}
+  }, { path: 'register', component: AuthentificationComponent },
+  { path: 'main', component: MainpageComponent }
 ];
 
 @NgModule({
